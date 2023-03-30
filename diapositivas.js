3dcfb14e89a8d5a472cbf68dc3108839bd18c715
Sayago-Asociados <129358796+Sayago-Asociados@users.noreply.github.com>
2023-03-29 23:56:53 -0500

    Add files via upload

---
+++ diapositivas.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+    },
+    speed: 1000,
+    slidesPerView: 'auto',
+  });
+  
